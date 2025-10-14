3c7178dd44bad07fecb28b7594a2e4ce6632661e
Hossam Hamdy <hhamdypro@gmail.com>
2025-10-14 07:25:52 +0300

    add changelog and root cause analysis documents

--- implementation/backend/src/main/java/com/conductor/core/action/db/enums/DatasourceType.java
+++
@@ -1,7 +0,0 @@
-package com.conductor.core.action.db.enums;
-
-public enum DatasourceType {
-    MYSQL,
-    POSTGRES,
-    ORACLE
-}
--- implementation/backend/src/main/java/com/conductor/core/action/db/repository/DatasourceRepository.java
+++
@@ -1,8 +0,0 @@
-package com.conductor.core.action.db.repository;
-
-import com.conductor.core.action.db.entity.DataSourceDef;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DatasourceRepository extends JpaRepository<DataSourceDef, String> { }
